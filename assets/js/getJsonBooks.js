9a4a524628fbfb93f1c3cd04418607fb2f6e82d7
NicHub <nicolas.jeanmonod@gmail.com>
2017-01-08 16:04:52 +0100

    IE

--- assets/js/getJsonBooks.js
+++ assets/js/getJsonBooks.js
@@ -11,25 +11,28 @@
     console.log( data );
 
     var htmlLivres = '';
-    for( var i=0; i<data.length; i++ ) {
-      console.log( data[ i ].image );
-      var htmlLivre = `
-      <div class="livre-out col-md-3 col-sm-4 col-sm-offset-0 col-xs-6 col-xs-offset-0 text-center">
-        <div class="row">
-          <a href="${ data[ i ].url }" target="_blank">
-            <div class="livre-in col-xs-10 col-xs-offset-1">
-              <div class="myfigure">
-                <img alt="..." src="images/livres/${ data[ i ].image }" class="img-responsive" />
-              </div>
+    try {
+      for( var i=0; i<data.length; i++ ) {
+        var htmlLivre = `
+          <div class="livre-out col-md-3 col-sm-4 col-sm-offset-0 col-xs-6 col-xs-offset-0 text-center">
+            <div class="row">
+              <a href="${ data[ i ].url }" target="_blank">
+                <div class="livre-in col-xs-10 col-xs-offset-1">
+                  <div class="myfigure">
+                    <img alt="..." src="images/livres/${ data[ i ].image }" class="img-responsive" />
+                  </div>
+                </div>
+              </a>
             </div>
-          </a>
-        </div>
-      </div>`;
-      htmlLivres += htmlLivre;
+          </div>`;
+        htmlLivres += htmlLivre;
+      }
+      $( "#row-livres" ).html( htmlLivres );
     }
-    $( "#row-livres" ).html( htmlLivres );
+    catch( err ) {
+      $( "#row-livres" ).html( "ERREUR" );
+    }
   }
 
 
 })();
-
