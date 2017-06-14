9bb6e4cd1cd69d8ba701b40f44b5113d13d49822
NicHub <nicolas.jeanmonod@gmail.com>
2017-06-14 10:09:06 +0200

    remplacé JQuery par JS standard

--- assets/js/getJsonBooks.js
+++ assets/js/getJsonBooks.js
@@ -1,39 +1,72 @@
 "use strict";
 
-( function() {
+var url = "portfolio_livres.json";
+var xhr = new XMLHttpRequest();
+var debug = false
 
-  $.getJSON( "portfolio_livres.json", function( data ) {
-    displayData( data );
-  });
+main();
 
-  function displayData( data ) {
-    // console.log( "data =" );
-    // console.log( data );
-    var htmlLivres = '';
-    try {
-      for( var i=0; i<data.length; i++ ) {
-        var htmlLivre = `
-          <div class="livre-out
-                      text-center
-                      col-md-3
-                      col-sm-4 col-sm-offset-0
-                      col-xs-6 col-xs-offset-0">
-            <div class="row">
-              <a href="${ data[ i ].url }" target="_blank" title="voir\n${ data[ i ].name }\nsur\n${ data[ i ].url }">
-                <div class="livre-figure
-                            z-depth-4">
-                  <img alt="${ data[ i ].name }" src="images/livres/${ data[ i ].image }" class="img-responsive" />
-                </div>
-              </a>
-            </div>
-          </div>`;
-        htmlLivres += htmlLivre;
-      }
-      $( "#row-livres" ).html( htmlLivres );
-    }
-    catch( err ) {
-      $( "#row-livres" ).html( "ERREUR" );
-    }
+function main()
+{
+  xhr.open( "GET", url );
+  xhr.send( null );
+
+  xhr.addEventListener( "load",  displayJSONinHTML );
+  xhr.addEventListener( "error", xhrDisplayStatus );
+}
+
+function xhrDisplayStatus()
+{
+  if( debug || xhr.status !== 200 )
+    console.log( "Code de réponse HTTP = " + xhr.status );
+}
+
+function displayJSONInConsole( responseJSON )
+{
+  console.log( xhr.responseText );
+  let JSONstringifyResponseJSON = JSON.stringify( responseJSON );
+  console.log( "JSONstringifyResponseJSON = " + JSONstringifyResponseJSON );
+  for( let item in responseJSON )
+  {
+    console.log( item );
+    console.log( responseJSON[ item ] );
   }
+}
 
-})();
+function displayJSONinHTML()
+{
+  xhrDisplayStatus();
+  let responseJSON = JSON.parse( xhr.responseText );
+  if( debug )
+    displayJSONInConsole( responseJSON );
+  let htmlTarget = document.getElementById( "htmlTargetID" );
+
+  let htmlElems = '';
+  try
+  {
+    for( var i=0; i<responseJSON.length; i++ )
+    {
+      var htmlElem = `
+        <div class="livre-out
+                    text-center
+                    col-md-3
+                    col-sm-4 col-sm-offset-0
+                    col-xs-6 col-xs-offset-0">
+          <div class="row">
+            <a href="${ responseJSON[ i ].url }" target="_blank" title="voir\n${ responseJSON[ i ].name }\nsur\n${ responseJSON[ i ].url }">
+              <div class="livre-figure
+                          z-depth-4">
+                <img alt="${ responseJSON[ i ].name }" src="images/livres/${ responseJSON[ i ].image }" class="img-responsive" />
+              </div>
+            </a>
+          </div>
+        </div>`;
+      htmlElems += htmlElem;
+    }
+    htmlTarget.innerHTML = htmlElems;
+  }
+  catch( err )
+  {
+    htmlTarget.innerHTML = "ERREUR displayJSONinHTML";
+  }
+}
--- assets/js/getJsonWebSites.js
+++ assets/js/getJsonWebSites.js
@@ -1,42 +1,74 @@
 "use strict";
 
-( function() {
+var url = "portfolio_sites_web.json";
+var xhr = new XMLHttpRequest();
+var debug = false
 
-  $.getJSON( "portfolio_sites_web.json", function( data ) {
-    displayData( data );
-  });
+main();
 
-  function displayData( data ) {
-    console.log( "data =" );
-    console.log( data );
+function main()
+{
+  xhr.open( "GET", url );
+  xhr.send( null );
 
-    var htmlSitesweb = '';
-    try {
-      for( var i=0; i<data.length; i++ ) {
-        var htmlSiteweb = `
-          <div class="siteweb-out
-                      col-md-6
-                      col-sm-6 col-sm-offset-0
-                      col-xs-10 col-xs-offset-1
-                      text-center">
-            <div class="row">
-              <a href="${ data[ i ].url }" target="_blank" title="${ data[ i ].name }">
-                <div class="siteweb-in col-xs-10 col-xs-offset-1">
-                  <div class="siteweb-figure
-                              z-depth-4">
-                    <img alt="${ data[ i ].name }" src="images/sites_web/${ data[ i ].image }" class="img-responsive" />
-                  </div>
-                </div>
-              </a>
-            </div>
-          </div>`;
-        htmlSitesweb += htmlSiteweb;
-      }
-      $( "#row-sitesweb" ).html( htmlSitesweb );
-    }
-    catch( err ) {
-      $( "#row-sitesweb" ).html( "ERREUR" );
-    }
+  xhr.addEventListener( "load",  displayJSONinHTML );
+  xhr.addEventListener( "error", xhrDisplayStatus );
+}
+
+function xhrDisplayStatus()
+{
+  if( debug || xhr.status !== 200 )
+    console.log( "Code de réponse HTTP = " + xhr.status );
+}
+
+function displayJSONInConsole( responseJSON )
+{
+  console.log( xhr.responseText );
+  let JSONstringifyResponseJSON = JSON.stringify( responseJSON );
+  console.log( "JSONstringifyResponseJSON = " + JSONstringifyResponseJSON );
+  for( let item in responseJSON )
+  {
+    console.log( item );
+    console.log( responseJSON[ item ] );
   }
+}
 
-})();
+function displayJSONinHTML()
+{
+  xhrDisplayStatus();
+  let responseJSON = JSON.parse( xhr.responseText );
+  if( debug )
+    displayJSONInConsole( responseJSON );
+  let htmlTarget = document.getElementById( "htmlTargetID" );
+
+  let htmlElems = '';
+  try
+  {
+    for( var i=0; i<responseJSON.length; i++ )
+    {
+      var htmlElem = `
+        <div class="siteweb-out
+                    col-md-6
+                    col-sm-6 col-sm-offset-0
+                    col-xs-10 col-xs-offset-1
+                    text-center">
+          <div class="row">
+            <a href="${ responseJSON[ i ].url }" target="_blank" title="${ responseJSON[ i ].name }">
+              <div class="siteweb-in col-xs-10 col-xs-offset-1">
+                <div class="siteweb-figure
+                            z-depth-4">
+                  <img alt="${ responseJSON[ i ].name }" src="images/sites_web/${ responseJSON[ i ].image }" class="img-responsive" />
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>`;
+      htmlElems += htmlElem;
+    }
+    htmlTarget.innerHTML = htmlElems;
+  }
+  catch( err )
+  {
+    htmlTarget.innerHTML = "ERREUR displayJSONinHTML";
+  }
+}
