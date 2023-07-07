8d32fa31ca6a4d3650317a31e91c10612f2549d0
NicHub <nicolas.jeanmonod@gmail.com>
2023-07-07 16:10:48 +0200

    Enlevé google analytics + mise à jour infos + refactoring

--- assets/js/getJsonBooks.js
+++ assets/js/getJsonBooks.js
@@ -1,72 +1,66 @@
 "use strict";
 
-var url = "portfolio_livres.json";
-var xhr = new XMLHttpRequest();
-var debug = false
+let url = "portfolio_livres.json";
+let xhr = new XMLHttpRequest();
+let debug = true;
 
 main();
 
-function main()
-{
-  xhr.open( "GET", url );
-  xhr.send( null );
+function main() {
+    xhr.open("GET", url);
+    xhr.send(null);
 
-  xhr.addEventListener( "load",  displayJSONinHTML );
-  xhr.addEventListener( "error", xhrDisplayStatus );
+    xhr.addEventListener("load", displayJSONinHTML);
+    xhr.addEventListener("error", xhrDisplayStatus);
 }
 
-function xhrDisplayStatus()
-{
-  if( debug || xhr.status !== 200 )
-    console.log( "Code de réponse HTTP = " + xhr.status );
+function xhrDisplayStatus() {
+    if (debug || xhr.status !== 200)
+        console.log("Code de réponse HTTP = " + xhr.status);
 }
 
-function displayJSONInConsole( responseJSON )
-{
-  console.log( xhr.responseText );
-  let JSONstringifyResponseJSON = JSON.stringify( responseJSON );
-  console.log( "JSONstringifyResponseJSON = " + JSONstringifyResponseJSON );
-  for( let item in responseJSON )
-  {
-    console.log( item );
-    console.log( responseJSON[ item ] );
-  }
+function displayJSONInConsole(responseJSON) {
+    if (!debug) return;
+    console.log(xhr.responseText);
+    const JSONstringifyResponseJSON = JSON.stringify(responseJSON);
+    console.log("JSONstringifyResponseJSON = " + JSONstringifyResponseJSON);
+    for (let item in responseJSON) {
+        console.log(item);
+        console.log(responseJSON[item]);
+    }
 }
 
-function displayJSONinHTML()
-{
-  xhrDisplayStatus();
-  let responseJSON = JSON.parse( xhr.responseText );
-  if( debug )
-    displayJSONInConsole( responseJSON );
-  let htmlTarget = document.getElementById( "htmlTargetID" );
-
-  let htmlElems = '';
-  try
-  {
-    for( var i=0; i<responseJSON.length; i++ )
-    {
-      var htmlElem = `
+function displayJSONinHTML() {
+    xhrDisplayStatus();
+    const responseJSON = JSON.parse(xhr.responseText);
+    displayJSONInConsole(responseJSON);
+    const htmlTarget = document.getElementById("htmlTargetID");
+    let htmlElems = "";
+    try {
+        for (let i = 0; i < responseJSON.length; i++) {
+            const a_tag_open =
+                responseJSON[i].availability === "https://schema.org/out-of-stock"
+                    ? `<a class="out-of-stock" title="Cet ouvrage n’est plus disponible.">`
+                    : `<a href="${responseJSON[i].url}" target="_blank" title="voir\n${responseJSON[i].name}\nsur\n${responseJSON[i].url}">`;
+            const htmlElem = `
         <div class="livre-out
                     text-center
                     col-md-3
                     col-sm-4 col-sm-offset-0
                     col-xs-6 col-xs-offset-0">
           <div class="row">
-            <a href="${ responseJSON[ i ].url }" target="_blank" title="voir\n${ responseJSON[ i ].name }\nsur\n${ responseJSON[ i ].url }">
+            ${a_tag_open}
               <div class="livre-figure
                           z-depth-4">
-                <img alt="${ responseJSON[ i ].name }" src="images/livres/${ responseJSON[ i ].image }" class="img-responsive" />
+                <img alt="${responseJSON[i].name}" src="images/livres/${responseJSON[i].image}" class="img-responsive" />
               </div>
             </a>
           </div>
         </div>`;
-      htmlElems += htmlElem;
+            htmlElems += htmlElem;
+        }
+        htmlTarget.innerHTML = htmlElems;
+    } catch (err) {
+        htmlTarget.innerHTML = "ERREUR displayJSONinHTML";
     }
-    htmlTarget.innerHTML = htmlElems;
-  }
-  catch( err )
-  {
-    htmlTarget.innerHTML = "ERREUR displayJSONinHTML";
-  }
 }
--- assets/js/getJsonWebSites.js
+++ assets/js/getJsonWebSites.js
@@ -1,74 +1,68 @@
 "use strict";
 
-var url = "portfolio_sites_web.json";
-var xhr = new XMLHttpRequest();
-var debug = false
+const url = "portfolio_sites_web.json";
+const xhr = new XMLHttpRequest();
+const debug = false;
 
 main();
 
-function main()
-{
-  xhr.open( "GET", url );
-  xhr.send( null );
+function main() {
+    xhr.open("GET", url);
+    xhr.send(null);
 
-  xhr.addEventListener( "load",  displayJSONinHTML );
-  xhr.addEventListener( "error", xhrDisplayStatus );
+    xhr.addEventListener("load", displayJSONinHTML);
+    xhr.addEventListener("error", xhrDisplayStatus);
 }
 
-function xhrDisplayStatus()
-{
-  if( debug || xhr.status !== 200 )
-    console.log( "Code de réponse HTTP = " + xhr.status );
+function xhrDisplayStatus() {
+    if (debug || xhr.status !== 200)
+        console.log("Code de réponse HTTP = " + xhr.status);
 }
 
-function displayJSONInConsole( responseJSON )
-{
-  console.log( xhr.responseText );
-  let JSONstringifyResponseJSON = JSON.stringify( responseJSON );
-  console.log( "JSONstringifyResponseJSON = " + JSONstringifyResponseJSON );
-  for( let item in responseJSON )
-  {
-    console.log( item );
-    console.log( responseJSON[ item ] );
-  }
+function displayJSONInConsole(responseJSON) {
+    if (!debug) return;
+    console.log(xhr.responseText);
+    const JSONstringifyResponseJSON = JSON.stringify(responseJSON);
+    console.log("JSONstringifyResponseJSON = " + JSONstringifyResponseJSON);
+    for (let item in responseJSON) {
+        console.log(item);
+        console.log(responseJSON[item]);
+    }
 }
 
-function displayJSONinHTML()
-{
-  xhrDisplayStatus();
-  let responseJSON = JSON.parse( xhr.responseText );
-  if( debug )
-    displayJSONInConsole( responseJSON );
-  let htmlTarget = document.getElementById( "htmlTargetID" );
-
-  let htmlElems = '';
-  try
-  {
-    for( var i=0; i<responseJSON.length; i++ )
-    {
-      var htmlElem = `
+function displayJSONinHTML() {
+    xhrDisplayStatus();
+    const responseJSON = JSON.parse(xhr.responseText);
+    displayJSONInConsole(responseJSON);
+    const htmlTarget = document.getElementById("htmlTargetID");
+    let htmlElems = "";
+    try {
+        for (let i = 0; i < responseJSON.length; i++) {
+            const a_tag_open =
+                responseJSON[i].validLink === false
+                    ? `<a class="valid-link" title="Ce site n’est plus en ligne ou a changé de webmaster.">`
+                    : `<a href="${responseJSON[i].url}" target="_blank" title="${responseJSON[i].name}">`;
+            const htmlElem = `
         <div class="siteweb-out
                     col-md-6
                     col-sm-6 col-sm-offset-0
                     col-xs-10 col-xs-offset-1
                     text-center">
           <div class="row">
-            <a href="${ responseJSON[ i ].url }" target="_blank" title="${ responseJSON[ i ].name }">
+            ${a_tag_open}
               <div class="siteweb-in col-xs-10 col-xs-offset-1">
                 <div class="siteweb-figure
                             z-depth-4">
-                  <img alt="${ responseJSON[ i ].name }" src="images/sites_web/${ responseJSON[ i ].image }" class="img-responsive" />
+                  <img alt="${responseJSON[i].name}" src="images/sites_web/${responseJSON[i].image}" class="img-responsive" />
                 </div>
               </div>
             </a>
           </div>
         </div>`;
-      htmlElems += htmlElem;
+            htmlElems += htmlElem;
+        }
+        htmlTarget.innerHTML = htmlElems;
+    } catch (err) {
+        htmlTarget.innerHTML = "ERREUR displayJSONinHTML";
     }
-    htmlTarget.innerHTML = htmlElems;
-  }
-  catch( err )
-  {
-    htmlTarget.innerHTML = "ERREUR displayJSONinHTML";
-  }
 }
