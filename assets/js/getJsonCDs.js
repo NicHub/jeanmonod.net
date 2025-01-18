8bf6fd20f47002b9e3421754b9b021aa67696435
NicHub <nicolas.jeanmonod@gmail.com>
2025-01-18 09:01:35 +0100

    compteur cds

--- assets/js/getJsonCDs.js
+++ assets/js/getJsonCDs.js
@@ -44,14 +44,17 @@ function displayJSONinHTML() {
                     col-md-3
                     col-sm-4 col-sm-offset-0
                     col-xs-12 col-xs-offset-0">
-          <div class="row">
-
-              <div class="cd-figure
-                          z-depth-4">
-                <img alt="${responseJSON[i].image}" src="images/cd/images/${responseJSON[i].image}" class="img-responsive" />
-                <div class="overlay">${responseJSON[i].image.split('.')[0]}</div>
+            <div class="row">
+              <div class="cd-figure z-depth-4">
+                <img
+                    alt="${responseJSON[i].image}"
+                    src="images/cd/images/${responseJSON[i].image}"
+                    class="img-responsive"
+                />
+                <div class="overlay">
+                    ${i}. ${responseJSON[i].image.split(".")[0]}
+                </div>
               </div>
-
           </div>
         </div>`;
             htmlElems += htmlElem;
